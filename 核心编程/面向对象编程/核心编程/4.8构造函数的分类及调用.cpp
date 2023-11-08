de55c1ffe9b5d78971044199751db7d5581405c3
YINCHUNHAO\ych <18610961996@163.com>
2023-11-08 22:22:50 +0800

    11月8日晚提交7日更改的文件

--- 核心编程/面向对象编程/核心编程/4.8构造函数的分类及调用.cpp
+++ 核心编程/面向对象编程/核心编程/4.8构造函数的分类及调用.cpp
@@ -1,42 +1,73 @@
 #include<iostream>
 using namespace std;
 
-//函数的构造
-//按照参数分类：默认构造函数和有参构造函数
-//按照类型分类：普通构造   拷贝构造
+//1.构造函数的分类及调用
+
+//分类
+//按照参数分类：  无参构造（默认构造）和有参构造
+//按照类型分类：  普通构造函数和拷贝构造函数
 class Person
 {
 public:
 	Person()
 	{
-		cout << "Person的无参构造函数" << endl;
+		cout << "Person的无参构造函数调用" << endl;
 	}
 	Person(int a)
 	{
 		age = a;
-		cout << "Person的有参构造函数" << endl;
+		cout << "Person的有参构造函数调用" << endl;
 	}
-
 	//拷贝构造函数
-	Person(const Person &P)
+	Person(const Person &p)
 	{
-		age = P.age;
-		cout << "Person的拷贝构造函数" << endl;
+		//将传入的人身上所有属性，拷贝到我身上
+		age = p.age;
+		cout << "Person的拷贝构造函数调用" << endl;
 	}
-
 	~Person()
 	{
-		cout << "Person的析构函数" << endl;
+		cout << "Person的析构函数调用" << endl;
 	}
-	
 	int age;
 
 };
 
+void test01()
+{
+	//1.括号法  优先使用
+	/*Person p1;
+	Person p2(10);
+	Person p3(p2);*/
 
-int main() {
+	//注意事项
+	// 调用默认函数的时候不要加（）
+	// Person p();  这样编译器认为是一个函数声明，不会认为在创建对象
+	
+	//2.显示法
+	Person p1;
+	Person p2 = Person(10);
+	Person p3 = Person(p2);
+	//Person(10);// 匿名对象： 特点  当前行执行结束后，系统会立即回收掉匿名对象。
+	//cout << "aaaa" << endl;
+	// 注意事项：不要利用拷贝构造函数 初始化匿名对象  编译器会认为 Person(p3) = Person p3 对象声明
+	// Person（p3）
+	
+	//3.隐式转换法	
+	Person p4 = 10; //相当于写了  Person p4 = Person(10);
+	Person p5 = p4;
+
+
+}
+
+//调用
+
+int main()
+{
+
+	test01();//默认构造函数调用
 
 
 	system("pause");
-		return 0;
+	return 0;
 }
